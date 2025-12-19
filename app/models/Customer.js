e7d688af7378cb0279cae01382c105c1f478a328
abhijith <abhijitht210@gmail.com>
2025-12-19 19:17:23 +0530

    added models: User, Customer, Case

---
+++ app/models/Customer.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+
+const customerSchema = new mongoose.Schema(
+  {
+    name: String,
+    contact_info: String,
+    status: { type: String, enum: ["active", "inactive"] },
+  },
+  { timestamps: true }
+);
